631447579774296dc48e2649e16c866fc57d0532
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-22 13:30:24 -0300

    arrival rate

--- api.py
+++ api.py
@@ -28,3 +28,25 @@ duplicate2 = etapa2(duplicate, "packet-duplicates")
 rtt2 = etapa2(rtt, "histogram-rtt")
 
 loss_rate2 = etapa2(loss_rate, "packet-loss-rate")
+
+
+def arrival(losses, sent, duplicate, time_window=84600):
+    sum_loss = 0
+    sum_sent = 0
+    sum_duplicate = 0
+    for element in losses:
+        sum_loss += element["val"]
+    
+    for x in range(len(sent)):
+        sum_sent += sent[x]["val"]
+
+    for y in range(len(duplicate)):
+        sum_duplicate += duplicate[y]["val"]
+
+    prob_arrival = 1 - (sum_loss/len(losses))
+    arrival = prob_arrival * (sum_sent-sum_duplicate)/time_window
+    return arrival
+
+print(arrival(loss_rate2, sent2, duplicate2))
+
+# 1711459620, 'val': 10.0}, {'ts': 1711460011, 'val': 10.0}, {'ts': 1711460375                  {'ts': 1711931837, 'val': 10.0}, {'ts': 1711932250, 'val': 10.0}, {'ts': 1711932367, 'val': 10.0}]
