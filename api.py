cac2178000b497fc7e231fd7bb34c6f0d3f0d265
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-23 10:54:55 -0300

    Update api.py

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ def etapa1(origem, dst, interface, event, time_range, bw_target_bandwidth=None):
 
 sent = etapa1("sp", "rj", "atraso", "packet-count-sent", "84600")
 duplicate = etapa1("sp", "rj", "atraso", "packet-duplicates", "84600")
-rtt =etapa1("sp", "rj", "atraso", "histogram-rtt", "84600")
+rtt = etapa1("sp", "rj", "atraso", "histogram-rtt", "84600")
 loss_rate = etapa1("sp", "rj", "atraso", "packet-loss-rate", "84600")
 
 
@@ -20,15 +20,6 @@ loss_rate = etapa1("sp", "rj", "atraso", "packet-loss-rate", "84600")
 def etapa2(uri, interface=""):
     uri = f"http://monipe-central.rnp.br/esmond/perfsonar/archive/{uri}/{interface}/base"
     return requests.get(uri, verify=False).json()
-
-sent2 = etapa2(sent, "packet-count-sent")
-
-duplicate2 = etapa2(duplicate, "packet-duplicates")
-
-rtt2 = etapa2(rtt, "histogram-rtt")
-
-loss_rate2 = etapa2(loss_rate, "packet-loss-rate")
-
 
 def arrival(losses, sent, duplicate, time_window=84600):
     sum_loss = 0
@@ -47,10 +38,23 @@ def arrival(losses, sent, duplicate, time_window=84600):
     arrival = prob_arrival * (sum_sent-sum_duplicate)/time_window
     return arrival
 
-print(arrival(loss_rate2, sent2, duplicate2))
-#copiloto
-# def delay(rtt, time_window=84600):
-#     sum_rtt = 0
-#     for element in rtt:
-#         sum_rtt += element["val"]
-#     return sum_rtt/time_window
+def service(rtt, time_window=84600):
+    sum_rtt = 0
+    for element in rtt:
+        sum_rtt += element["val"]
+    return sum_rtt/time_window
+
+sent2 = etapa2(sent, "packet-count-sent")
+
+duplicate2 = etapa2(duplicate, "packet-duplicates")
+
+rtt2 = etapa2(rtt, "histogram-rtt")
+
+loss_rate2 = etapa2(loss_rate, "packet-loss-rate")
+
+arrival_rate = arrival(loss_rate2, sent2, duplicate2)
+
+service_rate = service(rtt2)
+
+utilizacao = arrival_rate/service_rate
+packet_number 
