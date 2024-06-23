0e52a3e7dda68bf1e650e3f7130458d1c1bca1e4
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-23 15:32:50 -0300

    created main.py

--- api.py
+++ api.py
@@ -13,9 +13,6 @@ def etapa1(origem, dst, interface, event, time_range, bw_target_bandwidth=None):
 
     return requests.get(url, verify=False).json()[0]['metadata-key']
 
-
-
-
 def etapa2(uri, interface=""):
     uri = f"http://monipe-central.rnp.br/esmond/perfsonar/archive/{uri}/{interface}/base"
     return requests.get(uri, verify=False).json()
@@ -53,47 +50,7 @@ def waiting(rtt):
         sum_rtt += sum_val/sum_key
 
     return sum_rtt/len(rtt)
-#-----------------------------------------------------------------------------------------------------
 
-sent = etapa1("sp", "rj", "atraso", "packet-count-sent", "84600")
-
-duplicate = etapa1("sp", "rj", "atraso", "packet-duplicates", "84600")
-
-rtt = etapa1("sp", "rj", "atraso", "histogram-rtt", "84600")
-
-loss_rate = etapa1("sp", "rj", "atraso", "packet-loss-rate", "84600")
-
-#-----------------------------------------------------------------------------------------------------
-
-sent2 = etapa2(sent, "packet-count-sent")
-
-duplicate2 = etapa2(duplicate, "packet-duplicates")
-
-rtt2 = etapa2(rtt, "histogram-rtt")
-
-loss_rate2 = etapa2(loss_rate, "packet-loss-rate")
-
-#-----------------------------------------------------------------------------------------------------
-arrival_rate = arrival(loss_rate2, sent2, duplicate2)
-
-waiting_time = waiting(rtt2)
-
-service_time = waiting_time/(1+arrival_rate * waiting_time)
-
-utilizacao = arrival_rate * service_time
-
-packet_number = utilizacao / (1- utilizacao)
-
-
-# print(f'waiting_time: {waiting_time}')
-# print(f'arrival: {arrival_rate}')
-# print(f'service_time: {service_time}')
-# print(f'service_rate: {1/service_time}')
-# print(f'utilizacao: {utilizacao}')
-# print(f'packet_number: {packet_number}')
-
-#-----------------------------------------------------------------------------------------------------
-   
 def plot_histograma_sub_rotina(min_hist, max_hist, histogram_data):
 
     bin_edges = np.arange(min_hist , max_hist,  np.floor((max_hist - min_hist) / 5))
@@ -134,5 +91,3 @@ def plot_histograma(rtt):
     
     plot_histograma_sub_rotina(half_hist, max_hist, histogram_data)
  
-
-plot_histograma(rtt2)
