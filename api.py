40388d92b9ac9666350ae39ce833da72f5939d6c
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-23 14:40:04 -0300

    organizando

--- api.py
+++ api.py
@@ -10,10 +10,6 @@ def etapa1(origem, dst, interface, event, time_range, bw_target_bandwidth=None):
 
     return requests.get(url, verify=False).json()[0]['metadata-key']
 
-sent = etapa1("sp", "rj", "atraso", "packet-count-sent", "84600")
-duplicate = etapa1("sp", "rj", "atraso", "packet-duplicates", "84600")
-rtt = etapa1("sp", "rj", "atraso", "histogram-rtt", "84600")
-loss_rate = etapa1("sp", "rj", "atraso", "packet-loss-rate", "84600")
 
 
 
@@ -54,6 +50,17 @@ def waiting(rtt):
         sum_rtt += sum_val/sum_key
 
     return sum_rtt/len(rtt)
+#-----------------------------------------------------------------------------------------------------
+
+sent = etapa1("sp", "rj", "atraso", "packet-count-sent", "84600")
+
+duplicate = etapa1("sp", "rj", "atraso", "packet-duplicates", "84600")
+
+rtt = etapa1("sp", "rj", "atraso", "histogram-rtt", "84600")
+
+loss_rate = etapa1("sp", "rj", "atraso", "packet-loss-rate", "84600")
+
+#-----------------------------------------------------------------------------------------------------
 
 sent2 = etapa2(sent, "packet-count-sent")
 
@@ -63,19 +70,24 @@ rtt2 = etapa2(rtt, "histogram-rtt")
 
 loss_rate2 = etapa2(loss_rate, "packet-loss-rate")
 
+#-----------------------------------------------------------------------------------------------------
 arrival_rate = arrival(loss_rate2, sent2, duplicate2)
 
 waiting_time = waiting(rtt2)
-print(f'waiting_time: {waiting_time}')
-print(f'arrival: {arrival_rate}')
 
 service_time = waiting_time/(1+arrival_rate * waiting_time)
+
 utilizacao = arrival_rate * service_time
-# utilizacao = lambda*e[X]
-# packet_number = lambda*e[X]/ 1 - lambda*e[X]
+
 packet_number = utilizacao / (1- utilizacao)
 
-print(f'service_time: {service_time}')
-print(f'service_rate: {1/service_time}')
-print(f'utilizacao: {utilizacao}')
-print(f'packet_number: {packet_number}')
+
+# print(f'waiting_time: {waiting_time}')
+# print(f'arrival: {arrival_rate}')
+# print(f'service_time: {service_time}')
+# print(f'service_rate: {1/service_time}')
+# print(f'utilizacao: {utilizacao}')
+# print(f'packet_number: {packet_number}')
+
+#-----------------------------------------------------------------------------------------------------
+
