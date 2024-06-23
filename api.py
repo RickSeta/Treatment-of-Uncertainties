aa5b4dc518dd200782620eedbed8414dbdf0c0d5
RickSeta <54965077+RickSeta@users.noreply.github.com>
2024-06-23 15:29:54 -0300

    plotting

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 
 
+from collections import defaultdict
+import numpy as np
 import requests
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def etapa1(origem, dst, interface, event, time_range, bw_target_bandwidth=None):
     if bw_target_bandwidth:
@@ -90,4 +93,46 @@ packet_number = utilizacao / (1- utilizacao)
 # print(f'packet_number: {packet_number}')
 
 #-----------------------------------------------------------------------------------------------------
+   
+def plot_histograma_sub_rotina(min_hist, max_hist, histogram_data):
 
+    bin_edges = np.arange(min_hist , max_hist,  np.floor((max_hist - min_hist) / 5))
+    bin_labels = [f"{bin_edges[i]:.1f} - {bin_edges[i+1]:.1f}" for i in range(len(bin_edges)-1)]
+    binned_data = {label: 0 for label in bin_labels}
+
+    for value, frequencia in histogram_data.items():
+        for i in range(len(bin_edges) - 1):
+            if bin_edges[i] <= value < bin_edges[i + 1]:
+                binned_data[bin_labels[i]] += frequencia
+                break
+
+    df = pd.DataFrame(list(binned_data.items()), columns=['Intervalo', 'frequencia'])
+    df = df.sort_values('Intervalo')
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(df['Intervalo'], df['frequencia'], align='center')
+    plt.xlabel('Value Intervalos')
+    plt.ylabel('frequencia')
+    plt.title('Histograma rtt')
+    plt.xticks(rotation=45, ha='right')
+    plt.grid(True)
+    plt.savefig('histograma_rtt_' + str(min_hist) + '.png')
+    plt.show()
+
+def plot_histograma(rtt):
+    histogram_data = defaultdict(int)
+
+    for entry in rtt:
+        for key, value in entry["val"].items():
+            histogram_data[float(key)] += value
+
+    min_hist = min(histogram_data.keys()) - 1
+    max_hist = max(histogram_data.keys()) - 1
+    half_hist = max_hist / 2
+
+    plot_histograma_sub_rotina(min_hist, half_hist, histogram_data)
+    
+    plot_histograma_sub_rotina(half_hist, max_hist, histogram_data)
+ 
+
+plot_histograma(rtt2)
